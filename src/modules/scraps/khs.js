6ddcb443a864fff96642de31d7417eb995bae9be
ibra <ibrahimalanshor6@gmail.com>
2023-01-25 11:30:38 +0800

    scrap current semester khs

--- src/modules/scraps/khs.js
+++ src/modules/scraps/khs.js
@@ -12,11 +12,33 @@ async function getAllKhs(page) {
     console.log(`result saved: `, filename)
 }
 
+async function getCurrentKhs(page) {
+    await page.goto('https://sia.uty.ac.id/std/khssmt', {
+        waitUntil: 'load',
+        timeout: 0
+    })
+
+    const khsTable = await page.$('.table-responsive')
+
+    const result = await parseTable(page, khsTable, { headerRowNum: 1 })
+
+    const filename = await saveFile('khs-current-semester.json', JSON.stringify(result, null, 4))
+
+    console.log(`result saved: `, filename)
+}
+
 module.exports = async function khs(page, type) {
     await page.goto('https://sia.uty.ac.id/std/khs', {
         waitUntil: 'load',
         timeout: 0
     })
     
-    await getAllKhs(page)
+    switch (type) {
+        case 'current':
+            await getCurrentKhs(page)
+            break;
+        default:
+            await getAllKhs(page)
+            break;
+    }
 }
